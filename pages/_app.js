3a7150e9d2d09fb1d913fd27d2722f0fdf431b70
Celine <seelowbei@gmail.com>
2021-04-22 10:53:33 +0700

    remove antd package and setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 /* eslint-disable func-style */
 /* eslint-disable react/prop-types */
 import '../styles/globals.css'
-import '../styles/antd.less'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { Button } from 'antd'
 import Head from 'next/head'
 
 export default function Home() {
@@ -13,43 +12,13 @@ export default function Home() {
         />
       </Head>
 
-      <main className="bg-gray-500 flex flex-col justify-center w-screen h-screen items-center">
-        <h1 className="title">
+      <main className="bg-blue-100 flex flex-col justify-center w-screen h-screen items-center">
+        <h1 className="text-3xl">
           Next JS React Template
         </h1>
-
-        <Button type="primary">Test</Button>
       </main>
 
       <footer />
-
-      <style jsx>{`
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-      `}
-      </style>
-
-      <style
-        global
-        jsx
-      >{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}
-      </style>
     </div>
   )
 }
